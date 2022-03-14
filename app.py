088ec61f488dbb110e0b0a3ad3934e0e50a4adf3
LouisTsai-Csie <Q1030176@gmail.com>
2022-03-14 19:50:09 +0800

    new bot

--- app.py
+++ app.py
@@ -42,9 +42,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    line_bot_api.push_message(profile.user_id,TextSendMessage(text='Hi'))
 
 
 if __name__ == "__main__":
