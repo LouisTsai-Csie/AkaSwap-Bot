75ec4d234bb13610a9e6f993e79f60e27a008977
LouisTsai-Csie <Q1030176@gmail.com>
2022-03-14 19:53:21 +0800

    new bot

--- app.py
+++ app.py
@@ -49,5 +49,7 @@ def handle_message(event):
     line_bot_api.push_message(profile.user_id,TextSendMessage(text='Hi'))
 
 
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
