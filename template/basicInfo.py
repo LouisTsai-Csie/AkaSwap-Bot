f3396291d0c70b379b502b693660e6d5b36f96f2
LouisTsai-Csie <Q1030176@gmail.com>
2022-03-23 15:57:49 +0800

    test

--- template/basicInfo.py
+++ template/basicInfo.py
@@ -1,53 +1,54 @@
 from linebot.models import FlexSendMessage
 def basicInfo():
     content = FlexSendMessage(
-        {
-        "type": "bubble",
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-            {
-                "type": "text",
-                "text": "基本資訊設定",
-                "align": "center",
-                "style": "normal"
-            },
-            {
-                "type": "button",
-                "action": {
-                "type": "message",
-                "label": "地址輸入",
-                "text": "輸入地址"
+        alt_text= '基本設定',
+        content = {
+            "type": "bubble",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                {
+                    "type": "text",
+                    "text": "基本資訊設定",
+                    "align": "center",
+                    "style": "normal"
+                },
+                {
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "label": "地址輸入",
+                    "text": "輸入地址"
+                    }
+                },
+                {
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "text": "輸入信箱",
+                    "label": "信箱輸入"
+                    }
+                },
+                {
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "label": "追蹤作者輸入",
+                    "text": "輸入作者地址"
+                    }
+                },
+                {
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "label": "追蹤作品輸入",
+                    "text": "輸入作品ID"
+                    }
                 }
-            },
-            {
-                "type": "button",
-                "action": {
-                "type": "message",
-                "text": "輸入信箱",
-                "label": "信箱輸入"
-                }
-            },
-            {
-                "type": "button",
-                "action": {
-                "type": "message",
-                "label": "追蹤作者輸入",
-                "text": "輸入作者地址"
-                }
-            },
-            {
-                "type": "button",
-                "action": {
-                "type": "message",
-                "label": "追蹤作品輸入",
-                "text": "輸入作品ID"
-                }
+                ]
             }
-            ]
-        }
-        }
+            }
     )
     return content
 
