e566fa0adb60cc76910bdbd298afbc1fc491754a
LouisTsai-Csie <Q1030176@gmail.com>
2022-03-24 12:02:06 +0800

    fixed bug

--- template/flexHandler.py
+++ template/flexHandler.py
@@ -1,8 +1,9 @@
+from importlib.resources import contents
 from linebot.models import FlexSendMessage
 import json
 ### 基本設定的功能介面
 def basicInfo():
     flexMessage = json.load(open('template/basicInfo.json','r+',encoding='UTF-8'))
-    content = FlexSendMessage(alt_text= '基本介面',content=flexMessage)
+    content = FlexSendMessage(alt_text='基本介面',contents=flexMessage)
     return content
 ### 
